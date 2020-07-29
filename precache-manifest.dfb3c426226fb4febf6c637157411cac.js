8fa7caaf6cf628443e84365a599b43b0ad171767
James <github@twelve.pw>
2020-07-29 11:19:29 -0600

    Updates

---
+++ precache-manifest.dfb3c426226fb4febf6c637157411cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f4ec40c008739ad701e2b5e8b7d1cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a837eafb08cdad57ce7",
+    "url": "/static/css/main.79c2dd5a.chunk.css"
+  },
+  {
+    "revision": "0445ca920c64b6a99ebf",
+    "url": "/static/js/2.fd30edd2.chunk.js"
+  },
+  {
+    "revision": "7918f9b893310890f0a444ed75c67550",
+    "url": "/static/js/2.fd30edd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a837eafb08cdad57ce7",
+    "url": "/static/js/main.705273d2.chunk.js"
+  },
+  {
+    "revision": "50078e8834bd2de651cb",
+    "url": "/static/js/runtime-main.f331c618.js"
+  }
+]);
